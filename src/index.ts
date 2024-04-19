a855aed5cf817937c3a9478302283bc48a97fe71
david <Davids-MacBook-Pro.local>
2024-04-19 12:23:40 +1000

    Passed 4 to IV test

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 export function convert(number: number): string {
   const romanNumerals: Array<string> = [""];
   for (var i = 0; i < number; i++) {
-    romanNumerals.push("I");
+    if (i === 3) {
+      for (var j = 0; j < i; j++) {
+        romanNumerals.pop();
+      }
+      romanNumerals.push("I");
+      romanNumerals.push("V");
+    } else {
+      romanNumerals.push("I");
+    }
   }
   return romanNumerals.join("");
 }
