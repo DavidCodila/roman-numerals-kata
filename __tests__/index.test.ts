bf38f483dc253cfae820846422d2d4ae07d20d7b
david <Davids-MacBook-Pro.local>
2024-04-19 12:07:49 +1000

    Added passed 2 to II test

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -4,3 +4,7 @@ import { convert } from "../src/index";
 test("1 to I test", () => {
   expect(convert(1)).toEqual("I");
 });
+
+test("2 to II test", () => {
+  expect(convert(1)).toEqual("I");
+});
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export function convert(number: number): string {
+  if (number === 2) return "II";
   return "I";
 }
